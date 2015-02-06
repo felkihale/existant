e90418d1fd53ff3f1920421db9d6c660005a69c6
Fatiha Elkihale <f.elkihale@weedoit.fr>
2015-02-06 16:54:18 +0000

    first commit by me

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+echo " Welcome to the home page for the Test :) <br/>";
+echo " 1- First change <br/>";
+
+?>
