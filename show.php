a0dcb724cc8bd8768eeed4d0da03d3eccb811160
Fatiha Elkihale <f.elkihale@weedoit.fr>
2015-02-10 09:26:34 +0000

    msg for the first commit for the existant project

---
+++ show.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+echo " Show page :) <br/>";
+echo " La page de show.php  <br/>";
+
+?>
